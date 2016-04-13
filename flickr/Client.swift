6adf95e95cd2c619a4f1d208e5a385774be84622
iToguna <support@itgouna.com>
2016-04-14 00:13:40 +0100

    Realm taken off

--- flickr/Client.swift
+++ flickr/Client.swift
@@ -7,37 +7,9 @@
 //
 
 import UIKit
-import RealmSwift
 
 class Client: NSObject {
 
-    private var realmInstance:Realm?{
-        do {
-            return try Realm(configuration: Realm.Configuration(
-                path: Client.realmPath,
-                inMemoryIdentifier: nil,
-                encryptionKey: nil,
-                readOnly: false,
-                schemaVersion:1,
-                migrationBlock: { migration, oldSchemaVersion in
-                    print(migration)
-                    print(oldSchemaVersion)
-                },
-                objectTypes: nil))
-        }catch _ {
-            /// Panic mode initiated, delete realm instance
-            try! NSFileManager.defaultManager().removeItemAtPath(Client.realmPath)
-            return self.realmInstance
-        }
-    }
-    
-    private class var realmPath:String{
-        let dirContents = NSSearchPathForDirectoriesInDomains(.LibraryDirectory, .UserDomainMask, true)
-        if let libraryDirectory = dirContents.first {
-            return libraryDirectory.stringByAppendingString("/realm.realm")
-        }
-        return NSTemporaryDirectory().stringByAppendingString("realm.realm")
-    }
     class func backgroundThread(closure:()->()) {
         dispatch_async(dispatch_get_global_queue( QOS_CLASS_UTILITY, 0), {
             closure()
